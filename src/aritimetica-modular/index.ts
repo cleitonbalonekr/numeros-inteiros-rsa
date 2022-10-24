fc6997b846742c70f3c33f41c1bf59c5276e472f
Micaela Paes <micaelahimura@hotmail.com>
2022-10-24 20:41:59 -0200

    feat: add aula 5

---
+++ src/aritimetica-modular/index.ts
@@ -0,0 +1,57 @@
+// se for menor que a metade pega negativo
+// se for zero todos vão ser zero
+// se for 1 é um novo ciclo
+// busque resulado de menor magnitude
+// 3) escreva um algoritimo para calcular a potenciação x^y módilo n.
+// Aceleração: - quando achar valor 0 e quando achar valor 1
+type Input = {
+    modulo:number;
+    value:number
+}
+export function congruenciaModular({modulo,value}:Input){
+    const result = value%modulo;
+    if( Math.abs(result) > modulo/2){
+     return result > 0 ? result - modulo : result + modulo
+    }
+    return result
+}
+type PotenciaInput = {
+    modulo:number;
+    base:number
+    expoente:number
+}
+export function potenciaModular({base,expoente,modulo}:PotenciaInput){
+    const array = [1]
+    const mod1 = congruenciaModular({
+        modulo,
+        value:base
+    })
+     for (let index = 0; index < expoente; index++) {
+        const newValue = mod1* array[index];
+        const newMod = congruenciaModular({
+            modulo,
+            value:newValue
+        })
+        array.push(newMod)
+        if(newMod===0){
+            return array.fill(0,index,expoente)
+        }
+        if(newMod === 1){
+            return array
+        }
+        
+    }
+ 
+    return array
+}
+
+//console.log('reponse: x = 8 mod22  -> ',congruenciaModular({
+  //  modulo:22,
+   // value:8
+//}) )
+
+console.table(potenciaModular({
+    base:10,
+    expoente:8,
+    modulo:7
+}))
