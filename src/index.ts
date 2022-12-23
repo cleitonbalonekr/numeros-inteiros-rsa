e88e7be648b1491faea7bf6ca017bb7bc9737b0c
cleitonbalonekr <cleitonbaloneker@gmail.com>
2022-12-23 20:02:38 -0300

    feat: add encrytp and decrypt by file

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,30 @@ import fs from 'fs';
 import { encrypt } from './aula10';
 import { decryptRSA, getPrivateKey } from './aula11/descriptography';
 
+const ENCRYPTED_PATH = 'src/generated/encrypted/encrypted.txt';
+const DECRYPTED_PATH = 'src/generated/decrypted/decrypted.txt';
+
+function ecryptFile(n: number) {
+  const data = fs.readFileSync('src/plain.txt', 'utf8');
+  const encryptResponse = encrypt(data, n);
+  fs.writeFileSync(ENCRYPTED_PATH, encryptResponse.encrypted.join('-'));
+  return encryptResponse.e;
+}
+function decryptFile(n: number, e: number) {
+  const dataEncrypted = fs.readFileSync(ENCRYPTED_PATH, 'utf8');
+  const decryptedResponse = decryptRSA(
+    dataEncrypted.split('-') as unknown as number[],
+    n,
+    getPrivateKey(e, n)
+  );
+  fs.writeFileSync(DECRYPTED_PATH, decryptedResponse.decryptedMessage.join(''));
+}
+
 function main() {
   try {
-    const data = fs.readFileSync('src/plain.txt', 'utf8');
     const n = 209;
-    const encryptResponse = encrypt(data, n);
-    fs.writeFileSync(
-      'src/generated/encrypted/encrypted.txt',
-      encryptResponse.encrypted.join('')
-    );
-    const decryptedResponse = decryptRSA(
-      encryptResponse.encrypted,
-      n,
-      getPrivateKey(encryptResponse.e, n)
-    );
-    fs.writeFileSync(
-      'src/generated/decrypted/decrypted.txt',
-      decryptedResponse.decryptedMessage.join('')
-    );
+    const publicKey = ecryptFile(n);
+    decryptFile(n, publicKey);
   } catch (err) {
     console.error(err);
   }
